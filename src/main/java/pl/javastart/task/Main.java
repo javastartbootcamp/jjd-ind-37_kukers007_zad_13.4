56b89ca68d3d312c36af592295e39a0d58e78008
kukers007 <96911087+kukers007@users.noreply.github.com>
2023-04-03 22:18:14 +0200

    Solution 13.4

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -10,5 +10,19 @@ public class Main {
         CustomList<String> customListOfStrings = new CustomList();
         CustomList<Integer> customListOfIntegers = new CustomList();
 
+        customListOfStrings.add("Kasia1");
+        customListOfStrings.add("Kasia2");
+        customListOfStrings.add("Kasia3");
+        customListOfStrings.add("Kasia4");
+        customListOfStrings.add("Kasia5");
+        customListOfStrings.add("Kasia6");
+        customListOfStrings.add("Kasia7");
+        System.out.println(customListOfStrings.get(4));
+        System.out.println(customListOfStrings);
+        System.out.println(customListOfStrings);
+        System.out.println(customListOfStrings.size());
+        customListOfStrings.add(4, "Kasia101");
+        System.out.println(customListOfStrings);
+        System.out.println(customListOfStrings.size());
     }
 }
